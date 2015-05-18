edf283caf5a4e6c4d67a0984d77257508b272d71
ericksuarez <erick.suarez.buendia@gmail.com>
2015-05-18 12:27:19 -0500

    paquete detinado para todos los controladores

---
+++ src/com/ipn/edudown/johnlandongdown/controlador/PerfilesServlet.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.ipn.edudown.johnlandongdown.controlador;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author erick
+ *
+ */
+@SuppressWarnings("serial")
+public class PerfilesServlet extends HttpServlet {
+
+}
