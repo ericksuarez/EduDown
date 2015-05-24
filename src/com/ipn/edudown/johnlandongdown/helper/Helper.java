fa12cfcc6557a331aee0e0704ac3ad467daad580
ericksuarez <erick.suarez.buendia@mail.com>
2015-05-24 14:27:07 -0500

    paquete para fucniones helper para el sistema

---
+++ src/com/ipn/edudown/johnlandongdown/helper/Helper.java
@@ -0,0 +1,20 @@
+package com.ipn.edudown.johnlandongdown.helper;
+
+public class Helper {
+	
+	public String limpiaID(String clase, String classID) {
+		
+		String ID = null;
+		char letras[] = clase.toCharArray();
+
+		for (int i = 0; i < letras.length; i++) {
+			classID = classID.replace(letras[i], ' ');
+		}
+		classID = classID.replace('(', ' ');
+		classID = classID.replace(')', ' ');
+		
+		ID = classID.trim();
+		
+		return ID;
+	}
+}
