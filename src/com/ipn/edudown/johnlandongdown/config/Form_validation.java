9a82bcb7c950f3bcc28be16c4d078cb67457554c
ericksuarez <erick.suarez.buendia@mail.com>
2015-05-24 14:30:12 -0500

    nuevas validaciones para guardar alumnos y profesores

--- src/com/ipn/edudown/johnlandongdown/config/Form_validation.java
+++ src/com/ipn/edudown/johnlandongdown/config/Form_validation.java
@@ -8,22 +8,26 @@ private static final String iconError = "<i class='fa fa-question-circle'></i>";
 	
 	public String alumno(Alumno al) {
 
+		int cnt = 0;
 		String resp = "";
-		resp += (al.getNombre().length() > 0) ? null : iconError + " El campo Nombre es requerido <br>";
-		resp += (al.getApaterno().length() > 0) ? null : iconError + " El campo Apellido paterno es requerido <br>";
-		resp += (al.getAmaterno().length() > 0) ? null : iconError + " El campo Apellido materno es requerido <br>";
+		resp += (al.getNombre().length() > 0) ? cnt++ : iconError + " El campo Nombre es requerido <br>";
+		resp += (al.getApaterno().length() > 0) ? cnt++ : iconError + " El campo Apellido Paterno es requerido <br>";
+		resp += (al.getAmaterno().length() > 0) ? cnt++ : iconError + " El campo Apellido Materno es requerido <br>";
+		resp += (al.getUsuario().length() > 0) ? cnt++ : iconError + " El campo Usuario es requerido <br>";
 		
-		return resp;
+		return (cnt > 0) ? null : resp;
 	}
 
 	public String profesor(Profesor prof) {
 
+		int cnt = 0;
 		String resp = "";
-		resp += (prof.getNombre().length() > 0) ? null : iconError + " El campo Nombre es requerido <br>";
-		resp += (prof.getApaterno().length() > 0) ? null : iconError + " El campo Nombre es requerido <br>";
-		resp += (prof.getAmaterno().length() > 0) ? null : iconError + " El campo Nombre es requerido <br>";
-		resp += (prof.getEspecialidad().length() > 0) ? null : iconError + " El campo Nombre es requerido <br>";
+		resp += (prof.getNombre().length() > 0) ? cnt++ : iconError + " El campo Nombre es requerido <br>";
+		resp += (prof.getApaterno().length() > 0) ? cnt++ : iconError + " El campo Apellido Paterno es requerido <br>";
+		resp += (prof.getAmaterno().length() > 0) ? cnt++ : iconError + " El campo Apellido Materno es requerido <br>";
+		resp += (prof.getEspecialidad().length() > 0) ? cnt++ : iconError + " El campo Especialidad es requerido <br>";
+		resp += (prof.getEmail().length() > 0) ? cnt++ : iconError + " El campo Email es requerido <br>";
 		
-		return resp;
+		return (cnt > 0) ? null : resp;
 	}
 }
