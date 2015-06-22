d6e0a684ac8c580a3986d5ec97d33fbee5a01307
ericksuarez <erick.suarez.buendia@mail.com>
2015-06-21 23:51:07 -0500

    nuevos elementos para el juego de unir lineas

---
+++ war/js/juegos/completar/GameObject.js
@@ -0,0 +1,90 @@
+function GameObject (object, x, y, width, height) {
+
+    this.image = new Image ();
+    this.image.src = object.url;
+    this.id = object.id;
+    this.silaba = object.silaba;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.left = x;
+    this.top = y;
+    this.right = x+width;
+    this.bottom = y+height;
+    var clickFlag = false;
+    var xoffset;
+    var yoffset;
+    this.draw = function(ctx) {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        
+        if(this.silaba != "undefined" || this.silaba != ""){ //Para verificar si es un juego con imagenes o palabras
+        	ctx.font = "40px Arial";
+            ctx.fillStyle = "black";
+            ctx.fillText(this.silaba, (this.x+this.width*1/5), (this.y+this.height/2) );
+        }
+    };
+
+    this.click = function(evento) {
+        if (evento.clientX > this.x &&
+                evento.clientX < (this.x + this.width) &&
+                evento.clientY > this.y &&
+                evento.clientY < (this.y + this.height)
+                ) {
+
+            clickFlag = true;
+            xoffset = evento.clientX - this.x;
+            yoffset = evento.clientY - this.y;
+            return true;
+        }
+    };
+
+    this.move = function(evento) {
+        if (clickFlag == true) {
+            this.x = evento.clientX-xoffset;
+            this.y = evento.clientY-yoffset;
+            this.left = this.x;
+            this.top = this.y;
+            this.right = this.x + this.width;
+            this.bottom = this.y + this.height;
+        }
+    };
+    
+    this.collide = function (gameObject){
+
+       
+        return (
+                (   (gameObject.left   > this.left)&&
+                    (gameObject.left   < this.right)&&
+                    (gameObject.top    > this.top)&&
+                    (gameObject.top   < this.bottom)) ||
+
+                (   (gameObject.right  > this.left)&&
+                    (gameObject.right  < this.right)&&
+                    (gameObject.top    > this.top)&&
+                    (gameObject.top < this.bottom)) ||
+
+                (   (gameObject.right   > this.left)&&
+                    (gameObject.right  < this.right)&&
+                    (gameObject.bottom > this.top)&&
+                    (gameObject.bottom < this.bottom)) ||
+
+                (   (gameObject.left   > this.left)&&
+                    (gameObject.left  < this.right)&&
+                    (gameObject.bottom    > this.top)&&
+                    (gameObject.bottom    < this.bottom)) 
+
+            );
+    };
+
+    this.unclick = function (event){
+        clickFlag = false;
+        return clickFlag;
+    }
+
+    this.isClicked = function (){
+        return clickFlag ;
+    }
+    
+}
+
---
+++ war/relacionlineas.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<jsp:include page="common/header_Juego.jsp" />
+
+<!-- Parametros -->
+<input type="hidden" id="media" value='${jsonMedia}'> 
+<!--<input type="hidden" id="alumno" value='${alumno}'> --> 
+<input type="hidden" id="juego" value='${juego}'>
+<input type="hidden" id="semantico" value="${semantico}"> 
+<input type="hidden" id="idjuego" value=""> 
+<input type="hidden" id="idAvance" value=""> 
+
+<!-- Seccion del Juego Area del canvas -->
+<div class="row">
+	<div class="col-xs-1 col-md-1"></div>
+	<div class="col-xs-10 col-md-10">
+		<canvas id="canvas"> </canvas>
+	</div>
+	<div class="col-xs-1 col-md-1"></div>
+</div>
+
+<!-- Fin Seccion del Juego Area del canvas -->
+
+<!-- Script necesarios para el Juego -->
+<script src="js/cloud-endpoints/AvanceEndpoint.js"></script>
+        
+<script src="js/juegos/linea/GameObject.js"></script>
+<script src="js/juegos/linea/MatchGame.js"></script>
+<script src="js/juegos/linea/game.js"></script>
+
+<!-- Script necesarios para el Juego -->
+
+<jsp:include page="common/footer_Juego.jsp" />
